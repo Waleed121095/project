4394a0a36afb3520544d67aa23bae81ab3435180
Yasser Shaban <yaserbshaban@gmail.com>
2016-04-22 15:36:56 +0200

    my first commit

--- Classes/class_database.php
+++ Classes/class_database.php
@@ -8,21 +8,13 @@ class class_database {
 	private $password		= "estama"	
 	private $databaseName	= "CMSdatabase" // Content Managemnet System database
 	private $databaseConnection
-<<<<<<< HEAD
-=======
-
-	// TahaMagdy ^_^
-<<<<<<< HEAD
->>>>>>> d7c34c66d862f890d3fb0f26e358fd6b8f9114ee
-=======
 	// TahaMagdy ^_^
 
 	// TahaMagdy ^_^
 	// TahaMagdy ^_^
 	// TahaMagdy ^_^
 	// TahaMagdy ^_^
->>>>>>> 6324c487290ab42f9d3a91df96949ad223cdb68a
-
+//Yasser Test
 // Methods
 
 // Constructor
