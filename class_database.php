91b2684593c2e4ce10570d7d6d0c3cf4a9bd891e
TahaMgady <taha@taha-2.local>
2016-04-22 14:53:12 +0200

    I add a comment

---
+++ class_database.php
@@ -0,0 +1,25 @@
+<?php
+
+class class_database {
+
+// Attributes
+	private $host			= "localhost"
+	private $userName		= "taha"
+	private $password		= "estama"	
+	private $databaseName	= "CMSdatabase" // Content Managemnet System database
+	private $databaseConnection
+
+	// TahaMagdy
+
+// Methods
+
+// Constructor
+public function __construct ()
+	{
+		
+	} // end __construct	
+
+
+} // end class_database	
+
+?>
