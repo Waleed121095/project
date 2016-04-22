cc038a62028cae415045c8a2023f0138a01f5476
TahaMgady <taha@taha-2.local>
2016-04-22 14:23:34 +0200

    This is the Attributes; Methods Are being written.

---
+++ class_database.php
@@ -0,0 +1,24 @@
+<?php
+
+class class_database {
+
+// Attributes
+	private $host			= "localhost"
+	private $userName		= "taha"
+	private $password		= "estama"	
+	private $databaseName	= "CMSdatabase" // Content Managemnet System database
+	private $databaseConnection
+
+
+// Methods
+
+// Constructor
+public function __construct ()
+	{
+		
+	} // end __construct	
+
+
+} // end class_database	
+
+?>
