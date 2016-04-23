737322de12ab7c7811563c05e96d78c0072f8a8c
TahaMgady <taha@taha-2.local>
2016-04-23 16:15:04 +0200

    Yaser -_- ( Zriba )

---
+++ Classes/class_disk.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once 'class_stuff.php';
+class class_disk extends class_stuff{
+
+    public  function __construct() {
+ 
+        
+        } // End of constructor
+        public function retouch($text){
+
+            
+            
+    } // End of function
+    
+}       // End of class
---
+++ Classes/class_uploader.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'class_stuff.php';
+
+class class_uploader extends class_stuff{
+    public  function __construct() {
+ 
+        
+        } // End of constructor
+        public function publish($text){
+
+            
+    } // End of function
+    
+}       // End of class}
+
