b2fbcc20a40cfe01edd307851cee5d6e50e9ac53
TahaMgady <taha@taha-2.local>
2016-04-23 17:30:17 +0200

    Added some database functions!

--- Classes/class_database.php
+++ Classes/class_database.php
@@ -2,23 +2,92 @@
 
 class class_database {
 
-// Attributes
-	private $host			= "localhost"
-	private $userName		= "taha"
-	private $password		= "estama"	
-	private $databaseName	= "CMSdatabase" // Content Managemnet System database
-	private $databaseConnection
+// i) Attributes
+	private $host			= "localhost";
+	private $userName		= "taha"; 			// This is default value, You can overwrite is 
+	private $password		= "estama";			// This is default value, You can overwrite is 	
+	private $databaseName	= "CMSdatabase"; 	// Content Managemnet System database
+	private $databaseConnection;
 
 
 
 
-// Methods
+// ii) Methods
 
 // Constructor
-public function __construct ()
+public function __construct () 	// 1) Open mysql connection 
+								// 2) Select the database whose name is filled in the $databaseName Attribute
 	{
-		
+
+		$this->databaseConnection = $this->database_connect ( $this->host, $this->userName, $this->password );
+		$this->database_selectDB ( $this->databaseName );
+
 	} // end __construct	
+
+
+
+private function database_connect ( $host, $userName, $password )
+	{
+	
+		if 	(
+				$this->databaseConnection = mysql_connect ( $host, $userName, $password )
+		    ) // end if connection is established
+			return $databaseConnection;
+
+		else
+			die("Database error!");
+
+	} // end database_connect()	
+
+
+
+private function database_selectDB ( $databaseName )
+	{
+
+		if (
+				$select = mysql_select_db  ( $databaseName, $this->databaseConnection  )
+		   )  // end if database is selected
+			return $selectl; // boolean
+		else
+			die ( "No Database is selected" );
+
+	} // end database_selectDB()	
+
+
+
+private function database_close ()
+	{
+
+		if ( !mysql_close ( $this->connection ) )
+			die ( "Connection Close Error!" );
+
+	} // end database_close()	
+
+
+// Pre:  Non cleaned string is passed. { Queries, Passwords, ... }
+// Post: Clean string is returned.
+private function clean ( $string )
+	{
+
+		$string = trim 		   ( $string ); // Remove whitespaces form both sides.
+		$string = stripslashes ( $string ); // Removes scaping backslashes.
+		$string = strip_tags   ( $string ); // removes any tag  { < > }
+		$string = mysql_real_escape_string ( $string );
+ 
+		return $string;
+
+	} // end clean()
+
+
+// Pre:  The $database_query Must be cleaned by clean().
+// Post: Query result is returneda.
+private function database_query ( $database_query )
+	{
+
+		$result_query = mysql_query ( $database_query );
+		return $result_query;
+
+	} // end database_query()	
 
 
 } // end class_database	
