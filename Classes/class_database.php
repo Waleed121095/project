e6a0dd8a2f9adb5dd7de84ca530fe977f4efd448
Taha Magdy <t.tahamagdyt@outlook.com>
2016-04-26 17:30:28 +0300

    Edited some accessing modifiers and others.

--- Classes/class_database.php
+++ Classes/class_database.php
@@ -55,7 +55,7 @@ private function database_selectDB ( $databaseName )
 
 
 
-private function database_close ()
+public function database_close ()
 	{
 
 		if ( !mysql_close ( $this->connection ) )
@@ -69,7 +69,7 @@ private function database_close ()
 private function clean ( $string )
 	{
 
-		$string = trim 		   ( $string ); // Remove whitespaces form both sides.
+		$string = trim         ( $string ); // Remove whitespaces form both sides.
 		$string = stripslashes ( $string ); // Removes scaping backslashes.
 		$string = strip_tags   ( $string ); // removes any tag  { < > }
 		$string = mysql_real_escape_string ( $string );
@@ -81,14 +81,37 @@ private function clean ( $string )
 
 // Pre:  The $database_query Must be cleaned by clean().
 // Post: Query result is returneda.
-private function database_query ( $database_query )
+public function database_query ( $database_query )
 	{
 
 		$result_query = mysql_query ( $database_query );
 		return $result_query;
 
-	} // end database_query()	
+	} // end database_query()
 
+
+
+public function insert_query ( $tableName, $data_aray_assoc )
+	{
+		// Please, read this comment and remove it when you code!
+		// Hello Ahmed, You should take the following into your work.
+		// $data_aray_assoc is an associative array.
+			// Its key is the column name, and Its value is the data
+		// 1 ) This fucntion returns `true`  when insertion is correct.
+		 	// this means that you will execute the query.
+		// 2 ) This function returns `False` when insertion is no done.
+	} // end insert_query()
+
+
+
+public function update_query ( $tableName, $data_array_assoc, $where="")
+	{
+		// Please, read this comment and remove it when you code!
+		// 1 ) This fucntion execute the update quey after generating it, and Return `ture`
+		// else
+		// 2 ) This function returns `False` when insertion is no done.
+		
+	} // end update_query()
 
 } // end class_database	
 
