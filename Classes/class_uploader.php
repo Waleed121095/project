c74a79911f42fa5f1468de727fe6dfbee27ee018
abdoo <abdoo@192.168.1.7>
2016-05-02 23:47:55 +0200

    new abdulrahman

--- Classes/class_uploader.php
+++ Classes/class_uploader.php
@@ -1,15 +1,64 @@
 <?php
 include_once 'class_stuff.php';
-
-class class_uploader extends class_stuff{
-    public  function __construct() {
+include 'class_database.php';
+class class_uploader extends class_stuff
+{
+    public  function __construct()
+        {
  
         
         } // End of constructor
-        public function publish($text){
-
+    public function showAllRetouchedNews (  )
+    {        
+            $query = 'SELECT news.ID ,news.title , news.content, news.category , news.linkID ,news.viewsNumber,news.retouchState, Links.linksID ,Links.path FROM news JOIN Links ON news.linkID = Links.linksID WHERE news.retouchState = 1';
+            $db = new class_database();
+            $result = $db->database_query( $query );
+            return $result;   
             
-    } // End of function
+    }//end of showAllNews()
     
-}       // End of class}
+    public function showNews( $id )
+    {
+            $columns = 'content,ID';
+            $condition  = 'ID='.$id;
+            $tableName = 'news';
+            $db = new class_database();
+            $result = $db->select_query($columns, $tableName, $condition);
+            return $result;
+    }//end of showNews ()
+    
+    public function removeNews ( $id )
+    {
+      $tableName = 'news';
+      $data_array_assoc = array("retouchState" => "0");
+      $condition = 'ID ='.$id;
+      $db= new class_database();
+      $db->update_query($tableName, $data_array_assoc, $condition);
+      
+    }//end of removeNews
+
+    public function addAttachment($newsID,$fileName,$content,$fileType,$size)
+    {
+      
+        $tableName = 'NewsAttachment';
+        $data_aray_assoc = array ('id' =>$newsID, 'attachmentName'=>$fileName, 'attachmentType'=>$fileType, 'attachmentSize'=>$size, 'content'=>$content);
+        $db= new class_database();
+        $db->insert_query($tableName, $data_aray_assoc);
+        
+    }// end of addAttachment
+
+    public function publish( $NewsId , $uploderID ) //
+    {
+        $tableName = 'UploaderPublishNews';
+        $data_array_assoc = array ("uploaderID"=> $uploderID , "uploadedNewsID" => $NewsId);
+        $db= new class_database();
+        $result=  $db->insert_query($tableName, $data_array_assoc);
+        echo $result;
+        
+        
+    } // end of publish()
+    
+   
+} // End of class 
+
 
---
+++ View/showAllRetouchedNews.php
@@ -0,0 +1,83 @@
+
+<html>
+    <head>
+    <style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 5px;
+}
+
+</style>
+</head>
+
+    <body>
+      <table style="width:100%">
+            
+<?php
+include 'class_uploader.php';
+session_start();
+$_SESSION['id'] = 1;//this well be uploder or user session ID
+
+if(isset($_SESSION['id']))
+{
+ $id= $_SESSION['id'] ;  
+    
+echo '<tr>
+    <th>News ID </th>
+    <th>title</th>
+    <th>catogery</th>
+    <th>path</th>
+    <th>retouch State</th>
+    <th>views Number</th>
+    <th></th>
+  </tr>';
+ $re = new class_uploader();
+ $result = $re->showAllRetouchedNews();
+ $c= 0 ;
+ $flag = TRUE;
+ while ( $flag )
+ {
+   
+     
+     if( isset( $result[$c] ['title']))
+        {
+         
+           echo '<tr>
+            <td>'.$result[$c]['ID'].'</td>   
+            <td>'.$result [$c]['title'].'</td>
+            <td>'.$result[$c]['category'].'</td>
+            <td><a href="'.$result[$c]['path'].'"> '.$result[$c]['path'].'</td>
+            <td>'.$result[$c]['retouchState'].'</td>
+            <td>'.$result[$c]['viewsNumber'].'</td> 
+            <td>  
+           <form method="POST" action="showNews.php" >
+           <input type ="hidden" name="newsID" value='.$result[$c]['ID'].' >
+           <input type="submit" name="action" value="show">
+           </form> </td> 
+            </tr>' ;
+            /*echo $result[$c] ['title'];
+            echo $result[$c] ['title']."<a href='".$result[$c]['path']."'>show</a>'";
+            echo ' 
+            <form method="POST" action="showNews.php" >
+            <input type ="hidden" name="newsID" value="1" >
+            <input type="submit" name="action" value="show">
+            </form> ';*/
+            $flag = TRUE;
+            $c++;
+        }
+     else 
+        {
+          $flag = FALSE;
+        }
+    
+ }//end while
+}// end if isset session 
+ 
+ 
+?>
+        </table>
+</body>
+<html>
