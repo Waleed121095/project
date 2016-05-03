1df263cee566b8219357899daad66260c8578d21
abdoo <abdoo@192.168.1.7>
2016-05-03 02:04:00 +0200

    // i maked virtual session for user whith id = 1

--- View/showAllRetouchedNews.php
+++ View/showAllRetouchedNews.php
@@ -17,20 +17,20 @@ th, td {
       <table style="width:100%">
             
 <?php
-include 'class_uploader.php';
-session_start();
+include '../Classes/class_uploader.php';
+session_start();// session need to login() function ,I used initial user ID session to use here ,I will re implement the sessions again ^_^
 $_SESSION['id'] = 1;//this well be uploder or user session ID
 
 if(isset($_SESSION['id']))
 {
  $id= $_SESSION['id'] ;  
-    
+     
 echo '<tr>
     <th>News ID </th>
     <th>title</th>
     <th>catogery</th>
     <th>path</th>
-    <th>retouch State</th>
+    <th>Published State</th>
     <th>views Number</th>
     <th></th>
   </tr>';
@@ -50,7 +50,7 @@ echo '<tr>
             <td>'.$result [$c]['title'].'</td>
             <td>'.$result[$c]['category'].'</td>
             <td><a href="'.$result[$c]['path'].'"> '.$result[$c]['path'].'</td>
-            <td>'.$result[$c]['retouchState'].'</td>
+            <td>'.$result[$c]['appearState'].'</td>
             <td>'.$result[$c]['viewsNumber'].'</td> 
             <td>  
            <form method="POST" action="showNews.php" >
@@ -81,3 +81,4 @@ echo '<tr>
         </table>
 </body>
 <html>
+
