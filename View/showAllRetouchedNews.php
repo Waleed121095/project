722b9fe5cd0385feb4c56069bab0a19218f3ede9
abdoo <abdoo@192.168.1.7>
2016-05-03 02:31:13 +0200

    // i maked a virtual user id session

--- View/showAllRetouchedNews.php
+++ View/showAllRetouchedNews.php
@@ -1,6 +1,4 @@
-
-<html>
-    <head>
+d>
     <style>
 table, th, td {
     border: 1px solid black;
@@ -18,11 +16,7 @@ th, td {
             
 <?php
 include '../Classes/class_uploader.php';
-<<<<<<< HEAD
 session_start();// session need to login() function ,I used initial user ID session to use here ,I will re implement the sessions again ^_^
-=======
-session_start();
->>>>>>> 0b06f8cdc8f62faea1ac12b2246a260eb3c6616f
 $_SESSION['id'] = 1;//this well be uploder or user session ID
 
 if(isset($_SESSION['id']))
@@ -70,7 +64,90 @@ echo '<tr>
             <input type="submit" name="action" value="show">
             </form> ';*/
             $flag = TRUE;
-            $c++; // l2 Java! HAHAHAHAHAHAH peace TAHA
+            $c++;
+        }
+     else 
+        {
+          $flag = FALSE;
+        }
+    
+ }//end while
+}// end if isset session 
+ 
+ 
+?>
+        </table>
+</body>
+<html>
+
+<html>
+    <head>
+    <style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 5px;
+}
+
+</style>
+</head>
+
+    <body>
+      <table style="width:100%">
+            
+<?php
+include '../Classes/class_uploader.php';
+session_start();// session need to login() function ,I used initial user ID session to use here ,I will re implement the sessions again ^_^
+$_SESSION['id'] = 1;//this well be uploder or user session ID
+
+if(isset($_SESSION['id']))
+{
+ $id= $_SESSION['id'] ;  
+     
+echo '<tr>
+    <th>News ID </th>
+    <th>title</th>
+    <th>catogery</th>
+    <th>path</th>
+    <th>Published State</th>
+    <th>views Number</th>
+    <th></th>
+  </tr>';
+ $re = new class_uploader();
+ $result = $re->showAllRetouchedNews();
+ $c= 0 ;
+ $flag = TRUE;
+ while ( $flag )
+ {
+   
+     
+     if( isset( $result[$c] ['title']))
+        {
+         
+           echo '<tr>
+            <td>'.$result[$c]['ID'].'</td>   
+            <td>'.$result [$c]['title'].'</td>
+            <td>'.$result[$c]['category'].'</td>
+            <td><a href="'.$result[$c]['path'].'"> '.$result[$c]['path'].'</td>
+            <td>'.$result[$c]['appearState'].'</td>
+            <td>'.$result[$c]['viewsNumber'].'</td> 
+            <td>  
+           <form method="POST" action="showNews.php" >
+           <input type ="hidden" name="newsID" value='.$result[$c]['ID'].' >
+           <input type="submit" name="action" value="show">
+           </form> </td> 
+            </tr>' ;
+            /*echo $result[$c] ['title'];
+            echo $result[$c] ['title']."<a href='".$result[$c]['path']."'>show</a>'";
+            echo ' 
+            <form method="POST" action="showNews.php" >
+            <input type ="hidden" name="newsID" value="1" >
+            <input type="submit" name="action" value="show">
+            </form> ';*/
+            $flag = TRUE;
+            $c++;
         }
      else 
         {
